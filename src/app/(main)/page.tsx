7dfe8680c5c530e949ba11f8bc58993d840f6c92
Jefté Medeiros <jefte.medeiros8@gmail.com>
2024-01-07 01:13:33 -0500

    mode-toggle added

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,10 +1,11 @@
+import { ModeToggle } from "@/components/mode-toggle";
 import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div>
-      <p className='text-3xl text-indigo-500'>This is a protected route</p>
       <UserButton afterSignOutUrl="/" />
+      <ModeToggle />
     </div>
   )
 }
